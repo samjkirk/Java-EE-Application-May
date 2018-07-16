9b608294ff4c0bc91fafb7f1d5f7d819cb1b1b97
Sam Kirk <samueljkirk10@gmail.com>
2018-07-16 12:54:40 +0100

    refactor

--- src/main/java/business/iAccountsService.java
+++ src/main/java/business/iAccountsService.java
@@ -1,8 +1,11 @@
 package business;
 
+import domain.Account;
+
 public interface iAccountsService {
 	String findAllAccounts();
 	String create(String account);
 	String updateAccount(long id, String account);
 	String delete(long id);
+	Account findAccount(long id);
 }
